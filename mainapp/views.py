cd262952c76238ecf655313bf8b87c747b06b7d6
Vladimir Mikulitskii <mikulex@mail.ru>
2020-10-05 11:42:02 +0500

    Lesson_1.Working static site upd

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
+
 def main(request):
     return render(request, "mainapp/index.html")
+
 
 def products(request):
     return render(request, "mainapp/products.html")
 
+
 def contact(request):
     return render(request, "mainapp/contact.html")
-
-def productsdetail(request):
-    return render(request, "mainapp/productsdetail.html")
-
-
-# Create your views here.
