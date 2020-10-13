60a4c04e649e14d008fcda82f45736cfeab56118
vmikulitskii <66778244+vmikulitskii@users.noreply.github.com>
2020-10-13 14:34:42 +0500

    Lesson 3 (#3)
    Lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,33 +1,44 @@
+from django.conf import settings
 from django.shortcuts import render
 from datetime import datetime
+from django.utils import timezone
 import json
+from .models import Product, ProductCategory
 
 
 def main(request):
     title = 'главная страница'
-    with open('mainapp/products.json', 'r') as f:
-        products = json.load(f)
+    # with open('mainapp/products.json', 'r') as f:
+    #     products = json.load(f)
 
-    content = {"title": title, "products": products}
+    products = Product.objects.all()
+
+    content = {"title": title, "products": products,
+               'media_url': settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
-def products(request):
+def products(request, pk=None):
     title = 'товары'
-    with open('mainapp/links_menu.json', 'r') as f:
-        links_menu = json.load(f)
+    # with open('mainapp/links_menu.json', 'r') as f:
+    #     links_menu = json.load(f)
 
-    with open('mainapp/same_products.json', 'r') as f:
-        same_products = json.load(f)
+    # with open('mainapp/same_products.json', 'r') as f:
+    #     same_products = json.load(f)
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
 
     content = {"title": title, 'links_menu': links_menu,
-               "same_products": same_products}
+               "same_products": same_products,"media_url": settings.MEDIA_URL,}
+    
+    if pk:
+        print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = 'о нас'
-    visit_date = datetime.now()
+    visit_date = timezone.now()
 
     with open('mainapp/locations.json', 'r') as f:
         locations = json.load(f)
