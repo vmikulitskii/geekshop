0c7f2fb207ad69c37f55c783385d85424c7aa4a4
Vladimir Mikulitskii <mikulex@mail.ru>
2020-10-13 15:44:21 +0500

    Lesson 4. Data from files

--- mainapp/views.py
+++ mainapp/views.py
@@ -29,20 +29,17 @@ def products(request, pk=None):
     same_products = Product.objects.all()
 
     content = {"title": title, 'links_menu': links_menu,
-               "same_products": same_products,"media_url": settings.MEDIA_URL,}
-    
+               "same_products": same_products, "media_url": settings.MEDIA_URL, }
+
     if pk:
         print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    title = 'о нас'
+    title = "о нас"
     visit_date = timezone.now()
-
-    with open('mainapp/locations.json', 'r') as f:
-        locations = json.load(f)
-
+    locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date,
                "locations": locations}
     return render(request, "mainapp/contact.html", content)
