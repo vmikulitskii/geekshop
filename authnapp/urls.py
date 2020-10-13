cb0c7b25b9946c2143f2da413867d8d6f2fa5694
Vladimir Mikulitskii <mikulex@mail.ru>
2020-10-13 16:31:46 +0500

    Lesson 4. User login and logout

---
+++ authnapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+import authnapp.views as authnapp
+
+app_name = "authnapp"
+
+urlpatterns = [
+    path("login/", authnapp.login, name="login"),
+    path("logout/", authnapp.logout, name="logout"),
+]
