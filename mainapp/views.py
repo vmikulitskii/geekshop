9ab026cef8986f0d910acab01c6b8af1a573cbd4
Vladimir Mikulitskii <mikulex@mail.ru>
2020-10-15 14:51:19 +0500

    Lesson 5. Basket counter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
+from basketapp.views import basket
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Contact, Product, ProductCategory
+from basketapp.models import Basket
 
 
 def main(request):
@@ -24,6 +26,9 @@ def products(request, pk=None):
     # with open('mainapp/same_products.json', 'r') as f:
     #     same_products = json.load(f)
     links_menu = ProductCategory.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     if pk is not None:
         if pk == 0:
@@ -39,18 +44,19 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
+            "basket": basket
         }
         return render(request, "mainapp/products_list.html", content)
 
     same_products = Product.objects.all()
 
     content = {"title": title, 'links_menu': links_menu,
-               "same_products": same_products, "media_url": settings.MEDIA_URL, }
+               "same_products": same_products, "media_url": settings.MEDIA_URL, "basket": basket}
 
     if pk:
         print(f"User select category: {pk}")
     category = {'name': 'все'}
-    return render(request, "mainapp/products_list.html", content)
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
