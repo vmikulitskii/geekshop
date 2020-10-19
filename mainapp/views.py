0e5b6eaa708e47462ea0a1f7c32f996f9f8f62b5
vmikulitskii <66778244+vmikulitskii@users.noreply.github.com>
2020-10-19 13:08:04 +0500

    Lesson 5 (#5)
    * Lesson 5.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,9 @@
+from basketapp.views import basket
 from django.conf import settings
-from django.shortcuts import render
-from datetime import datetime
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-import json
 from .models import Contact, Product, ProductCategory
+from basketapp.models import Basket
 
 
 def main(request):
@@ -11,7 +11,7 @@ def main(request):
     # with open('mainapp/products.json', 'r') as f:
     #     products = json.load(f)
 
-    products = Product.objects.all()
+    products = Product.objects.all()[:4]
 
     content = {"title": title, "products": products,
                'media_url': settings.MEDIA_URL}
@@ -26,13 +26,45 @@ def products(request, pk=None):
     # with open('mainapp/same_products.json', 'r') as f:
     #     same_products = json.load(f)
     links_menu = ProductCategory.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        total_quantity = Basket.total_quantity(basket)
+        total_price = Basket.total_price(basket)
+    else:
+        total_quantity = 0
+        total_price = 0
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by('price')
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+            "basket": basket,
+            "total_quantity":total_quantity,
+            "total_price":total_price,
+
+        }
+        return render(request, "mainapp/products_list.html", content)
+
     same_products = Product.objects.all()
 
     content = {"title": title, 'links_menu': links_menu,
-               "same_products": same_products, "media_url": settings.MEDIA_URL, }
+               "same_products": same_products, "media_url": settings.MEDIA_URL, "basket": basket, "total_quantity":total_quantity,
+            "total_price":total_price,}
 
     if pk:
         print(f"User select category: {pk}")
+    category = {'name': 'все'}
     return render(request, "mainapp/products.html", content)
 
 
@@ -40,5 +72,6 @@ def contact(request):
     title = "о нас"
     visit_date = timezone.now()
     locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {"title": title, "visit_date": visit_date,
+               "locations": locations}
     return render(request, "mainapp/contact.html", content)
