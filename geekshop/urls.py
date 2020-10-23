66abc6d3370dfcae45f099a766dd0cd8758bbb30
Vladimir Mikulitskii <mikulex@mail.ru>
2020-10-23 10:34:28 +0500

    lesson_7. Adminapp.

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -7,12 +7,12 @@ import mainapp.views as mainapp
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", mainapp.main, name="main"),
     path("products/", include("mainapp.urls",namespace='products')),
     path("contact/", mainapp.contact,name="contact"),
     path("auth/", include("authnapp.urls", namespace="auth")),
     path("basket/", include("basketapp.urls", namespace="basket")),
+    path('admin/',include("adminapp.urls",namespace='admin')),
     ]
 
 
