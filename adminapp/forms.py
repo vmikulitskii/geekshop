1bd7f0e8cb10ff58d72425b97725a11c105a6f76
vmikulitskii <66778244+vmikulitskii@users.noreply.github.com>
2020-10-30 16:00:50 +0500

    Lesson 7 (#7)
    * lesson_7

---
+++ adminapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from mainapp.models import ProductCategory
+from authnapp.forms import ShopUserEditForm
+from authnapp.models import ShopUser
+
+class ShopUserAdminEditForm(ShopUserEditForm):
+    class Meta:
+        model = ShopUser
+        fields = "__all__"
+
+
+class ProductCategoryEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ""
+
+    class Meta:
+        model = ProductCategory
+        fields = "__all__"   
