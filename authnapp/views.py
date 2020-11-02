45c1a34f660f47809463c1b2bf8613f5124d9b93
Vladimir Mikulitskii <mikulex@mail.ru>
2020-11-02 18:56:04 +0500

    lesson_1. Send email to file.

--- authnapp/views.py
+++ authnapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 from django.conf import settings
+from django.core.mail import send_mail
 
 from authnapp.forms import ShopUserEditForm, ShopUserLoginForm, ShopUserRegisterForm
+from authnapp.models import ShopUser
 
 # Create your views here.
 
@@ -41,13 +43,50 @@ def register(request):
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
 
         if register_form.is_valid():
-            register_form.save()
+            user = register_form.save()
+            if send_verify_mail(user):
+                print('Сообщение для продтверждения регистрации отправлено')
+                return HttpResponseRedirect(reverse('auth:login'))
+            print("ошибка отправки сообщения для подтверждения регистрации")
             return HttpResponseRedirect(reverse("auth:login"))
+            
     else:
         register_form = ShopUserRegisterForm()
 
     content = {"title": title, "register_form": register_form}
     return render(request, "authnapp/register.html", content)
+
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify',args=[user.email,user.activation_key])
+
+    title = f'Подтверждение учетной записи {user.username}'
+    message = f"Для подтверждения учетной записи {user.username} \
+    на портале {settings.DOMAIN_NAME} перейдите по ссылке: \
+    \n{settings.DOMAIN_NAME}{verify_link}"
+
+    print(f"from: {settings.EMAIL_HOST_USER}, to: {user.email}")
+    return send_mail(title,message,settings.EMAIL_HOST_USER,[user.email],fail_silently=False,)
+
+def verify(request,email,activation_key):
+    try:
+        user = ShopUser.objects.get(email=email)
+        if user.activation_key == activation_key and not user.is_activation_key_expiered():
+            print(f'user {user.username} is activated')
+            user.is_active = True
+            user.save()
+            auth.login(request,user)
+
+            return render(request,'authapp/verification.html')
+        else:
+            print(f"error activation user: {user}")
+            return render(request, "authapp/verification.html")
+    except Exception as e:
+        print(f"error activation user : {e.args}")
+
+    return HttpResponseRedirect(reverse("main"))
+
+        
+
 
 
 def edit(request):
