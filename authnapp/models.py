329e4c97a828149755259d75fbd44ffe60a9990e
Vladimir Mikulitskii <mikulex@mail.ru>
2020-11-10 16:44:47 +0500

    lesson_2. User's profile

--- authnapp/models.py
+++ authnapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import timedelta
+from django.db.models.deletion import CASCADE
 from django.utils.timezone import now
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -19,4 +22,25 @@ class ShopUser(AbstractUser):
         return True
 
 
-        
+class ShopUserProfile(models.Model):
+    MALE = "M"
+    FEMALE = "W"
+
+    GENDER_CHOICES = (
+        (MALE, "М"),
+        (FEMALE, "Ж"),
+    )
+
+    user = models.OneToOneField(ShopUser,unique=True, null=False, db_index=True, on_delete=CASCADE)
+    tagline = models.CharField(verbose_name="теги", max_length=128, blank=True)
+    aboutMe = models.TextField(verbose_name="о себе", max_length=512, blank=True)
+    gender = models.CharField(verbose_name="пол", max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
