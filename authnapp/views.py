9dcdedec471b67438023371e7d7e016a556c29bd
Vladimir Mikulitskii <mikulex@mail.ru>
2020-11-10 17:02:41 +0500

    lesson_2. User's profile edit

--- authnapp/views.py
+++ authnapp/views.py
@@ -3,8 +3,9 @@ from django.contrib import auth
 from django.urls import reverse
 from django.conf import settings
 from django.core.mail import send_mail
+from django.db import transaction
 
-from authnapp.forms import ShopUserEditForm, ShopUserLoginForm, ShopUserRegisterForm
+from authnapp.forms import ShopUserEditForm, ShopUserLoginForm, ShopUserProfileEditForm, ShopUserRegisterForm, ShopUserEditForm
 from authnapp.models import ShopUser
 
 # Create your views here.
@@ -89,19 +90,21 @@ def verify(request,email,activation_key):
         
 
 
-
+@transaction.atomic
 def edit(request):
     title = "редактирование"
 
     if request.method == "POST":
         edit_form = ShopUserEditForm(
             request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        profile_form = ShopUserProfileEditForm(request.POST, request.FILES, instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse("auth:edit"))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
+        profile_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
 
-    content = {"title": title, "edit_form": edit_form,
+    content = {"title": title, "edit_form": edit_form, "profile_form": profile_form,
                "media_url": settings.MEDIA_URL}
     return render(request, "authnapp/edit.html", content)
