7e9ebc535740d880909d1cfbb25b24e15c24eedc
Vladimir Mikulitskii <mikulex@mail.ru>
2020-11-17 15:32:16 +0500

    lesson_4. Product's count

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,17 +1,25 @@
 from django.conf import settings
-from django.db import models, reset_queries
+from django.db import models
+from django.shortcuts import get_object_or_404
 
 from mainapp.models import Product
 
 
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE, related_name="basket")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(
-        verbose_name="количество", default=0)
-    add_datetime = models.DateTimeField(
-        verbose_name="время добавления", auto_now_add=True)
+    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
 
     @property
     def product_cost(self):
@@ -32,9 +40,25 @@ class Basket(models.Model):
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
 
-    def __str__(self):
-        return f'{self.user} - {self.product.name} - {self.quantity}'
-
     @staticmethod
     def get_items(user):
         return Basket.objects.filter(user=user).order_by("product__category")
+
+    @staticmethod
+    def get_item(pk):
+        return get_object_or_404(Basket, pk=pk)
+
+    # Object's saving method
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
+
+    # Object's deleting method
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
